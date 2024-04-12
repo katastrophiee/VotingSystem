1043b65470819a124654191f6f86d0f33b65e2cf
kaytl <kaytl@DESKTOP-TI4LR7A>
2024-04-12 21:31:07 +0100

    Add document and error popup localisation

---
+++ Components/Pages/Shared/DocumentRenderer.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using VotingSystem.API.DTO.DbModels;
+
+namespace VotingSystem.Components.Pages.Shared;
+
+public partial class DocumentRenderer
+{
+    [Parameter]
+    public Document Document { get; set; }
+
+    [Inject]
+    public IStringLocalizer<DocumentRenderer> Localizer { get; set; }
+}
---
+++ Components/Pages/Shared/ErrorPopup.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using VotingSystem.API.DTO.ErrorHandling;
+
+namespace VotingSystem.Components.Pages.Shared;
+
+public partial class ErrorPopup
+{
+    [Parameter]
+    public List<ErrorResponse> Errors { get; set; }
+
+    [Inject]
+    public IStringLocalizer<ErrorPopup> Localizer { get; set; }
+}
