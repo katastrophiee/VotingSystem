926d77e209f60fb03f500ed5ca6f44c43275e3d6
kaytl <kaytl@DESKTOP-TI4LR7A>
2024-04-12 20:57:53 +0100

    Add admin home localisation

--- Components/Pages/Admin/AdminHome.razor.cs
+++ Components/Pages/Admin/AdminHome.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using VotingSystem.Services;
 
 namespace VotingSystem.Components.Pages.Admin;
@@ -11,6 +12,9 @@ public partial class AdminHome
 
     [Inject]
     public ILocalStorageService _localStorage { get; set; }
+
+    [Inject]
+    public IStringLocalizer<AdminHome> Localizer { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
