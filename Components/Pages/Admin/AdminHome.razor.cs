3d1dfd94c6b77773fe08a526e05686341d85fabb
kaytl <kaytl@DESKTOP-TI4LR7A>
2024-04-21 16:09:40 +0100

    Add admin home page

--- Components/Pages/Admin/AdminHome.razor.cs
+++ Components/Pages/Admin/AdminHome.razor.cs
@@ -1,6 +1,10 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
+using System.Threading.Tasks;
+using VotingSystem.API.DTO.ErrorHandling;
+using VotingSystem.API.DTO.Requests.Admin;
+using VotingSystem.API.DTO.Responses.Admin;
 using VotingSystem.Services;
 
 namespace VotingSystem.Components.Pages.Admin;
@@ -16,8 +20,26 @@ public partial class AdminHome
     [Inject]
     public IStringLocalizer<AdminHome> Localizer { get; set; }
 
+    public List<ErrorResponse> Errors { get; set; } = [];
+
+    public int AdminId { get; set; }
+
+    public List<AdminGetTaskResponse>? AdminTasks { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
-        
+        AdminId = await _localStorage.GetItemAsync<int>("adminUserId");
+
+        var getAdminAssignedToTasks = new AdminGetTasksRequest()
+        {
+            AdminId = AdminId,
+            AssignedToAdminId = AdminId
+        };
+
+        var tasks = await ApiRequestService.SendAsync<IEnumerable<AdminGetTaskResponse>>("Admin/PostGetTasks", HttpMethod.Post, getAdminAssignedToTasks);
+        if (tasks.Error == null)
+            AdminTasks = tasks.Data.ToList();
+        else
+            Errors.Add(tasks.Error);
     }
 }
